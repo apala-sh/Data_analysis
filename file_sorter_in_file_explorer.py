4efa64e07323a758ed6d101062cf9675b15690cd
apala-sh <apseven17@gmail.com>
2026-09-19 19:11:45 +0530

    auto sorting files based on extensions DONE!!!!!!

--- file_sorter_in_file_explorer.py
+++ file_sorter_in_file_explorer.py
@@ -1,21 +1,16 @@
 import os, shutil #shutil allows high level file operations in file explorer
 from pathlib import Path
+from os.path import isfile, join 
 
 path = r"C:/Users/Apala/Documents/Data_analysis/py_file_sorter_test/"
 #the 'r'==> reads it as a raw string instead of interpreting all the 
 #backslash and colon stuff
 
 #check if the folder has any files and print em
-files = os.listdir(path) #lists all files in the directory
+#lists only the files 
+files = [f for f in os.listdir(path) if isfile(join(path, f))] 
 #print(files)
 
-'''
-#file types and check if the folder already exists, if not create it 
-file_types = ['png', 'jpg', 'xls', 'doc', 'pdf', 'txt']
-for i in file_types:
-    if not os.path.exists(path + i):
-        os.makedirs(path + i)
-'''
 
 #get the extensions of all the files in the folder 
 file_ext = []
@@ -27,7 +22,7 @@ for i in files:
     elif file_path not in file_ext:
         file_ext.append(file_path)
 
-print(file_ext)
+#print(file_ext)
 
 #create folders with extension names
 for i in file_ext:
@@ -35,7 +30,12 @@ for i in file_ext:
         os.makedirs(path + i)
 
 #match file extension to folder names and move the files 
+for file in files:
+    for ext in file_ext:
+        if Path(file).suffix == ext:
+            shutil.move(path+file, path + f'{ext}/' + file)
 
+        
     
 
 
