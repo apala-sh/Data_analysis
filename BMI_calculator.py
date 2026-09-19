9bd1983f4fe310e39a4b52b8dd15ba7a28f8e11e
apala-sh <apseven17@gmail.com>
2026-09-19 11:33:07 +0530

    bmi cla done

--- BMI_calculator.py
+++ BMI_calculator.py
@@ -4,3 +4,13 @@ weight = float(input("Enter your weight in kg: "))
 height = float(input("Enter your height in meter: "))
 BMI = (weight) / (height**2)
 print(BMI)
+if (BMI<18.5):
+    print("Underweight")
+elif (18.5 <= BMI <= 24.9):
+    print("Healthy weight")
+elif (25 <= BMI <= 29.9):
+    print("Overweight")
+elif (BMI>=30):
+    print("Obese")
+else:
+    print("Enter valid inputs")
