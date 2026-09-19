ca2294e2c22dccb46a6e47e3e444c87315c56e8b
apala-sh <apseven17@gmail.com>
2026-09-19 11:18:02 +0530

    coded, run on linux later

---
+++ BMI_calculator.py
@@ -0,0 +1,6 @@
+# BMI = (weight in kg)/(height in m)^2
+
+weight = float(input("Enter your weight in kg: "))
+height = float(input("Enter your height in meter: "))
+BMI = (weight) / (height**2)
+print(BMI)
